090ad0023cf9dfa5d6141e6d2784b5352f949020
GHOST11111100 <mirdodoevsasha@gmail.com>
2022-04-20 15:45:07 +0300

    change #14

--- telegram/streamNotify/src/js/about.js
+++ telegram/streamNotify/src/js/about.js
@@ -1,12 +1,5 @@
-var tag = document.body.getElementsByTagName('h1')[0]
-var tag2 = document.body.getElementsByTagName('h2')[0]
-
 window.Telegram.WebApp.ready()
 
-let params = (new URL(document.location)).searchParams; 
-let data = JSON.parse(params.get('data'))
-
-for (let i in data) {
-
-    console.log(data[ i ])
+function close() {
+    window.Telegram.WebApp.close()
 }
