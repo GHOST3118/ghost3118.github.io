34abb6ae1d024952956a5b0bf0bd0c55512781a2
GHOST11111100 <mirdodoevsasha@gmail.com>
2022-04-20 15:20:31 +0300

    change #12

--- telegram/streamNotify/src/js/about.js
+++ telegram/streamNotify/src/js/about.js
@@ -3,5 +3,8 @@ var tag2 = document.body.getElementsByTagName('h2')[0]
 
 window.Telegram.WebApp.ready()
 
+let params = (new URL(document.location)).searchParams; 
+console.log(params.get("data"));
+
 tag.innerText = window.Telegram.WebApp.initDataUnsafe.user.first_name || 'NoName'
 tag2.innerText = window.Telegram.WebApp.initDataUnsafe.user.last_name || ''
