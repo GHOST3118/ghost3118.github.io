d42f8173691bdbc9ea2d9370c4f2138e26034746
GHOST11111100 <mirdodoevsasha@gmail.com>
2023-04-28 22:12:35 +0300

    ssss

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-import express from "https://esm.sh/express";
+// Start listening on port 8080 of localhost.
+const server = Deno.listen({ port: 8080 });
+console.log("File server running on http://localhost:8080/");
 
-const app = express()
+for await (const conn of server) {
+  handleHttp(conn).catch(console.error);
+}
 
-app.use('/', express.static('dist'))
-app.get('/', (req, res) => {
-    res.sendFile('./dist/index.html')
-})
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    // Use the request pathname as filepath
+    const url = new URL(requestEvent.request.url);
+    const filepath = decodeURIComponent(url.pathname);
 
+    // Try opening the file
+    let file;
+    try {
+      file = await Deno.open("." + filepath, { read: true });
+    } catch {
+      // If the file cannot be opened, return a "404 Not Found" response
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+      continue;
+    }
 
-app.listen(80)
+    // Build a readable stream so the file doesn't have to be fully loaded into
+    // memory while we send it
+    const readableStream = file.readable;
+
+    // Build and send the response
+    const response = new Response(readableStream);
+    await requestEvent.respondWith(response);
+  }
+}
